946fb4eebda07f6ec647032ffe7d511042404118
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2026-06-20 15:30:47 -0300

    Antecessor e sucessor

---
+++ Antecessor e sucessor.py
@@ -0,0 +1,5 @@
+numero = int(input('Digite um número: '))
+antecessor = numero - 1
+sucessor = numero + 1
+
+print('O antecessor do número {0} é: {1}. \n O sucessor do número {0}, é: {2}'.format(numero, antecessor, sucessor))
