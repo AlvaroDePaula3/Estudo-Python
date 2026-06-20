2dd567b2b546ca26c41804ab99bcc575687b066a
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2026-06-20 15:38:45 -0300

    Dobro, triplo e raiz quadrada

---
+++ dobro, triplo e raiz quadrada.py
@@ -0,0 +1,6 @@
+numero = int(input('Digite um número: '))
+dobro = numero * 2
+triplo = numero * 3
+raiz= numero**(1/2)
+
+print('O número é: {}, \n O dobro desse número é: {}, \n O triplo do número é: {}, \n A raiz quadrada é: {}'.format(numero, dobro, triplo, raiz))
