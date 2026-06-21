6f97b7f997e5e9bc12865f47034a8f2fbd31a177
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2026-06-21 12:54:48 -0300

    desconto

---
+++ desconto.py
@@ -0,0 +1,5 @@
+precoCheio = float(input("Digite o valor do produto: R$"))
+
+precoDesconto = precoCheio-(precoCheio * 0.05)
+
+print("O preço do produto com 5% de desconto é {}".format(precoDesconto))
