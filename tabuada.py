2b8636e0a71477d40537ecbe60d6d4759e2b20a1
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2026-06-21 12:48:27 -0300

    Tabuada

---
+++ tabuada.py
@@ -0,0 +1,14 @@
+numeroTabuada = int(input('Digite um número '))
+
+print("{} x {} = {}".format(numeroTabuada, 1, numeroTabuada*1))
+print("{} x {} = {}".format(numeroTabuada, 2, numeroTabuada*2))
+print("{} x {} = {}".format(numeroTabuada, 3, numeroTabuada*3))
+print("{} x {} = {}".format(numeroTabuada, 4, numeroTabuada*4))
+print("{} x {} = {}".format(numeroTabuada, 5, numeroTabuada*5))
+print("{} x {} = {}".format(numeroTabuada, 6, numeroTabuada*6))
+print("{} x {} = {}".format(numeroTabuada, 7, numeroTabuada*7))
+print("{} x {} = {}".format(numeroTabuada, 8, numeroTabuada*8))
+print("{} x {} = {}".format(numeroTabuada, 9, numeroTabuada*9))
+print("{} x {} = {}".format(numeroTabuada, 10, numeroTabuada*10))
+
+# print('A tabuada do número:', numeroTabuada, 'é: \n', numeroTabuada*1, '\n', numeroTabuada*2, '\n', numeroTabuada*3, '\n',numeroTabuada*4, '\n',numeroTabuada*5, '\n',numeroTabuada*6, '\n',numeroTabuada*7, '\n',numeroTabuada*8, '\n',numeroTabuada*9, '\n',numeroTabuada*10 )
