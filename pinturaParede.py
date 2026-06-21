47e449adc0aae78c7151b3bad59f743cd8da6940
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2026-06-21 12:52:26 -0300

    tinta necessária para pintar uma parede

---
+++ pinturaParede.py
@@ -0,0 +1,9 @@
+largura = float(input("Largura: "))
+altura = float(input("Altura: "))
+areaDaParede = largura*altura
+
+print("A área da parede é {:.2f}".format(areaDaParede))
+
+tintaNecessaria = areaDaParede/2
+
+print("A quantidade de tinta necessária para pintar a parede é: {:.2f}".format(tintaNecessaria))
